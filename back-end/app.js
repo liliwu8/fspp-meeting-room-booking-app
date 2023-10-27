fe683f6496316ec143d9658c017cd2cf45cd6303
lily08 <liliangelxd@gmail.com>
2023-10-27 16:55:56 -0400

    create folders of controllers,db,and queries, and connect server and app

---
+++ back-end/app.js
@@ -0,0 +1,25 @@
+// DEPENDENCIES
+
+const express = require('express');
+const cors = require('cors');
+
+
+// CONFIGURATION
+const app = express();
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.send('Welcome to meeting room booking app');
+});
+
+
+app.get('*', (req, res) => {
+  res.status(404).send('Not found!');
+});
+
+// EXPORT
+module.exports = app;
